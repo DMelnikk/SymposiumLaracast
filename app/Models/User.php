9d53e82884e22e64d1fb5d68d9a4e6cbe49b5c5c
DMelnikk <dmitry.melnik001@mail.ru>
2025-09-28 18:28:27 +0300

    Created factories and seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function talks()
+    {
+        return $this->hasMany(Talk::class);
+    }
 }
